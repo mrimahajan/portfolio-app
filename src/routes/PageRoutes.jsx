81e752a6d295deeaa83882ac49b55345530abd7b
Mrinal-Mahajan <mahajanmrinal2013@gmail.com>
2024-02-03 15:36:15 +0530

    final changes for static page

--- src/routes/PageRoutes.jsx
+++ src/routes/PageRoutes.jsx
@@ -1,19 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import route from "./route.json";
 import MainLayout from "../components/layouts/MainLayout";
-<<<<<<< HEAD
-=======
-import Home from "../pages/Home";
->>>>>>> a743a50d64b246d73bf89832a1a2750c509b7465
 import About from "../pages/About";
 import Resume from "../pages/Resume";
 import Portfolio from "../pages/Portfolio";
 import ContactUs from "../pages/ContactUs";
 import Error from "../pages/Error";
-<<<<<<< HEAD
 import Home from "../pages/Home";
-=======
->>>>>>> a743a50d64b246d73bf89832a1a2750c509b7465
 
 const PageRoutes = () => {
   return (
